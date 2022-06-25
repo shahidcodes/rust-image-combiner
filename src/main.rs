5914207f3bcc9527bb032fa1c5c92a54b307b500
Shahid Kamal <shahidkamal08@gmail.com>
2022-06-25 20:43:48 +0530

    does not work

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+mod args;
+
+use args::Args;
+use image::{
+    imageops::FilterType::Triangle, io::Reader, DynamicImage, GenericImageView, ImageFormat,
+};
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    println!("{:?}", args);
+    let (image_1, image_1_format) = find_image_path(args.image_1);
+    let (image_2, image_2_format) = find_image_path(args.image_2);
+
+    if image_1_format != image_2_format {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    println!("making standard size");
+
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+
+    let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+
+    println!("Combining images");
+
+    let combined_data = combine_images(image_1, image_2);
+
+    output.set_data(combined_data)?;
+
+    println!("saving to file");
+
+    image::save_buffer_with_format(
+        output.name,
+        &output.data,
+        output.w,
+        output.h,
+        image::ColorType::Rgba8,
+        image_1_format,
+    )
+    .unwrap();
+
+    Ok(())
+}
+
+fn find_image_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader = Reader::open(path).unwrap();
+    let image_format = image_reader.format().unwrap();
+    let image = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (w, h) = get_smallest_dimensions(image_1.dimensions(), image_2.dimensions());
+
+    if image_2.dimensions() == (w, h) {
+        (image_1.resize_exact(w, h, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(w, h, Triangle))
+    }
+}
+
+#[derive(Debug)]
+struct FloatingImage {
+    w: u32,
+    h: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+impl FloatingImage {
+    fn new(w: u32, h: u32, name: String) -> Self {
+        let buffer_cap = 3_655_744;
+        let buffer = Vec::with_capacity(buffer_cap);
+        FloatingImage {
+            w: w,
+            h: h,
+            data: buffer,
+            name: name,
+        }
+    }
+
+    fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+        if data.len() > self.data.capacity() {
+            return Err(ImageDataErrors::BufferTooSmall);
+        }
+
+        self.data = data;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+    BufferTooSmall,
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgb8().into_vec();
+    let vec_2 = image_2.to_rgb8().into_vec();
+    alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let mut combined_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+        } else {
+            combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+        }
+        i += 4;
+    }
+    combined_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba = Vec::new();
+
+    for i in start..=end {
+        let val = match vec.get(i) {
+            Some(d) => *d,
+            None => panic!("Index out bound"),
+        };
+        rgba.push(val);
+    }
+    rgba
+}
